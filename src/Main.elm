a51a91a00070b153777b3331e1b98f590c008647
Jason Huang <jasonhuang20035@gmail.com>
2022-03-06 16:16:50 -0500

    initialize final project
    Co-authored-by: Felix Fong <fongf2@mcmaster.ca>
    Co-authored-by: himikheal <himikheal@users.noreply.github.com>
    Co-authored-by: Sarah Simionescu <sarahsimionescu@users.noreply.github.com>

--- src/Main.elm
+++ src/Main.elm
@@ -1,285 +1,131 @@
 module Main exposing (..)
 
-import Fongf2.RocketGame
+import Browser
+import Browser.Dom as Dom
+import Browser.Events as Browser
+import Element as E
+import Element.Background as Background
+import Element.Border as Border
+import Element.Events as Events
+import Element.Font as Font
+import Element.Input as Input
+import Element.Region as Region
 import GraphicSVG exposing (..)
 import GraphicSVG.EllieApp exposing (..)
-import Huanj168.WinScreen
-import Nagii.RocketMoving
-
-
-myShapes model =
-    case model.state of
-        Start ->
-            [ Nagii.RocketMoving.myShapes model
-                |> group
-            , group
-                [ roundedRect 40 20 5
-                    |> filled green
-                , text "ToAstroid"
-                    |> centered
-                    |> size 8
-                    |> filled black
-                    |> move ( 0, -3 )
-                ]
-                |> notifyTap ToAstroid
-            ]
-
-        AstroidScreen ->
-            [ if model.keyV == -1 then
-                fire
-                    |> move ( 0, 12 )
-                    |> move model.posL
-
-              else
-                circle 15
-                    |> filled (rgba 0 0 0 0)
-            , if model.keyV == 1 then
-                fire
-                    |> scale -1
-                    |> move ( 0, -12 )
-                    |> move model.posL
-
-              else
-                circle 15
-                    |> filled (rgba 0 0 0 0)
-            , if model.keyH == -1 then
-                fire
-                    |> rotate (degrees 90)
-                    |> scale -1
-                    |> move ( 12, 0 )
-                    |> move model.posL
-
-              else
-                circle 15
-                    |> filled (rgba 0 0 0 0)
-            , if model.keyH == 1 then
-                fire
-                    |> rotate (degrees 270)
-                    |> scale -1
-                    |> move ( -12, 0 )
-                    |> move model.posL
-
-              else
-                circle 15
-                    |> filled (rgba 0 0 0 0)
-            , drawAstroid (rgb 255 0 0) model.posL model.velL
-            , group
-                [ roundedRect 40 20 5
-                    |> filled green
-                , text "ToRocket"
-                    |> centered
-                    |> size 8
-                    |> filled black
-                    |> move ( 0, -3 )
-                ]
-                |> move ( 0, -50 )
-                |> notifyTap ToRocket
-            ]
-
-        RocketScreen ->
-            [ group <| Fongf2.RocketGame.myShapes model
-            , group
-                [ roundedRect 40 20 5
-                    |> filled green
-                , text "ToWin"
-                    |> centered
-                    |> size 8
-                    |> filled black
-                    |> move ( 0, -3 )
-                ]
-                |> move ( -25, -25 )
-                |> notifyTap ToWin
-            , group
-                [ roundedRect 40 20 5
-                    |> filled green
-                , text "ToAstroid"
-                    |> centered
-                    |> size 8
-                    |> filled black
-                    |> move ( 0, -3 )
-                ]
-                |> move ( 25, -25 )
-                |> notifyTap ToAstroid
-            ]
-
-        WinScreen ->
-            [ text "WinScreen"
-                |> centered
-                |> filled black
-            , group
-                [ roundedRect 40 20 5
-                    |> filled green
-                , text "ToRocket"
-                    |> centered
-                    |> size 8
-                    |> filled black
-                    |> move ( 0, -3 )
-                ]
-                |> move ( 0, -25 )
-                |> notifyTap ToRocket
-            ]
-
-
-
--- Huanj168.WinScreen.myShapes model
-
-
-type Msg
-    = Tick Float GetKeyState
-    | ToRocket
-    | ToWin
-    | ToAstroid
-
-
-type State
-    = Start
-    | AstroidScreen
-    | RocketScreen
-    | WinScreen
-
-
-update msg model =
-    case msg of
-        Tick t ( _, accelR, accelL ) ->
-            case model.state of
-                AstroidScreen ->
-                    { model
-                        | time = t
-                        , posL = timeStep model.velL model.posL
-                        , velL = timeStep accelR model.velL
-                        , keyV = Tuple.second accelR
-                        , keyH = Tuple.first accelR
-                    }
-
-                otherwise ->
-                    { model
-                        | time = t
-                        , posR = timeStep model.velR model.posR
-                        , velR =
-                            let
-                                positiveThrust =
-                                    if Tuple.second accelR < 0 then
-                                        0
-
-                                    else
-                                        Tuple.second accelR
-
-                                accel =
-                                    ( positiveThrust * cos -model.angleR
-                                    , positiveThrust * sin -model.angleR
-                                    )
-                            in
-                            timeStep accel model.velR
-                        , angleR = 0.1 * Tuple.first accelR + model.angleR
-                    }
-
-        ToRocket ->
-            case model.state of
-                AstroidScreen ->
-                    { model | state = RocketScreen }
-
-                WinScreen ->
-                    { model | state = RocketScreen }
-
-                otherwise ->
-                    model
-
-        ToWin ->
-            case model.state of
-                RocketScreen ->
-                    { model | state = WinScreen }
-
-                otherwise ->
-                    model
-
-        ToAstroid ->
-            case model.state of
-                Start ->
-                    { model | state = AstroidScreen }
-
-                RocketScreen ->
-                    { model | state = AstroidScreen }
-
-                otherwise ->
-                    model
+import GraphicSVG.Widget as Widget
+import Task
+import Time
+import Dict exposing (..)
 
 
 type alias Model =
     { time : Float
+    , width : Int
+    , height : Int
+    , widgetModel : Widget.Model 
     , state : State
-    , keyH : Int
-    , keyV : Int
-    , astroidScreenModel : Fongf2.RocketGame.Model
-    , winScreenModel : Huanj168.WinScreen.Model
+    , sidebarState : SidebarState
     }
 
+type State
+    = Animating     -- make up your own states
+    | NotAnimating
 
-type alias Point =
-    ( Float, Float )
+type SidebarState
+    = Edit
+    | Run
 
+type Msg
+    = WindowResize Int Int -- update the size of the window
+    | Tick Float
 
-init =
+type alias Graph =
+  Dict String Node
+
+-- contains the following info:
+-- label, (x, y coords), [connected nodes]
+type alias Node = 
+  { coordinates : (Int, Int)
+  , connections : List String
+  }
+
+initW = Widget.init 300 100 "gsvgTop"
+
+initialModel : Model
+initialModel =
     { time = 0
-    , state = Start
-    , posL = ( 0, 0 )
-    , velL = ( 0, 0 )
-    , velR = ( 0, 0 )
-    , keyH = 0
-    , keyV = 0
-    , angleR = 0
+    , width = 600
+    , height = 1024
+    , widgetModel = Tuple.first initW
+    , state = NotAnimating
+    , sidebarState = Edit
     }
 
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        WindowResize width height ->
+            ({ model | width = width, height = height }
+            , Cmd.none)
+        Tick t ->
+            ({ model | time = t }, Cmd.none)
 
-main =
-    gameApp Tick { model = init, view = view, update = update, title = "Game Slot" }
-
-
+view : Model -> Browser.Document Msg
 view model =
-    collage 192 128 (myShapes model)
+    if model.width < 550 then
+        phoneView model 
+    else
+        tabletView model 
 
+phoneView : Model -> Browser.Document Msg
+phoneView model =
+        { title = "My Elm UI + Widget App"
+        , body =    [E.layout []
+                        (E.text "Hello phone!")
+                    ]
+        }
 
-fire =
-    isosceles 5 10
-        |> filled (rgb 0 0 255)
+tabletView : Model -> Browser.Document Msg
+tabletView model =
+        { title = "My Elm UI + Widget App"
+        , body =    [E.layout [
+                        E.height E.fill
+                      ]
+                        (E.row [E.height E.fill] [
+                          (E.column [E.width <| E.px (model.width // 5) 
+                            , E.height E.fill
+                            , Background.color <| E.rgb255 240 240 240
+                            , E.padding 10
+                            , Font.size 14
+                            ] 
+                            [ E.paragraph 
+                              [ E.paddingEach {bottom = 20, left = 0, right = 0, top = 0}] 
+                              [E.text "Drag Me for a ..."]
+                            , E.paragraph [E.centerX] [E.text "Node"]
+                            ]
+                            )
+                          , (E.text "World")])
+                    ]
+        }
 
+renderNode id node =
+  [id, node.coordinates]
 
-astroidShape =
-    circle 12
-        --circular shape of astroid
-        |> filled gray
+main : Program () Model Msg
+main =
+    Browser.document
+        { init = \ flags -> (initialModel, Task.perform ( \ vp -> WindowResize (round vp.viewport.width) (round vp.viewport.height)) Dom.getViewport)
+        , view = view
+        , update = update
+        , subscriptions = \ model -> case model.state of 
+                                        NotAnimating -> Browser.onResize WindowResize
+                                        Animating ->
+                                            Sub.batch 
+                                                [ Browser.onResize WindowResize
+                                                , Browser.onAnimationFrame (Time.posixToMillis 
+                                                                            >> toFloat 
+                                                                            >> (\ t -> 0.001 * t ) 
+                                                                            >> Tick)
+                                                ]
+        }
 
-
-astroidPat =
-    group
-        --circular crators design on astroid
-        [ circle 3
-            |> filled (rgb 115 115 120)
-            |> move ( 10, 6 )
-        , circle 4
-            |> filled (rgb 115 115 120)
-            |> move ( -5, 2 )
-        , circle 3.5
-            |> filled (rgb 115 115 120)
-            |> move ( 5, -9 )
-        , circle 5
-            |> filled (rgb 115 115 120)
-            |> move ( -9, -9 )
-        , circle 2
-            |> filled (rgb 115 115 120)
-            |> move ( -5, -6 )
-        , circle 2.5
-            |> filled (rgb 115 115 120)
-            |> move ( -3, 5 )
-        , circle 1.5
-            |> filled (rgb 115 115 120)
-            |> move ( 5, -5 )
-        , circle 1
-            |> filled (rgb 115 115 120)
-            |> move ( 5, 10 )
-        , circle 1.5
-            |> filled (rgb 115 115 120)
-            |> move ( 7, 7 )
-        , circle 2
-            |> filled (rgb 115 115 120)
-            |> move ( 8, 3 )
-        ]
